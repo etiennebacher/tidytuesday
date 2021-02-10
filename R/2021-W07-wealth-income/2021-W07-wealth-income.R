a937cd05ee8b1f21592c6b3cd281767c082fa914
etiennebacher <you@example.com>
2021-02-10 17:21:44 +0100

    produce intermediary plots

--- R/2021-W07-wealth-income/2021-W07-wealth-income.R
+++ R/2021-W07-wealth-income/2021-W07-wealth-income.R
@@ -5,15 +5,17 @@ library(hrbrthemes)
 library(waffle)
 library(ggrepel)
 library(glue)
+library(ggtext)
+library(patchwork)
 
 tuesdata <- tidytuesdayR::tt_load(2021, week = 7)
 
 
 # top_5 <-
-plot_owner_perc <- function(race) {
+plot_owner_perc <- function(ethnic) {
 
   tuesdata$home_owner %>%
-    filter(race == race) %>%
+    filter(race == ethnic) %>%
     mutate(owner = round(mean(home_owner_pct, na.rm = T)*100, 0),
            non_owner = 100-owner) %>%
     ungroup() %>%
@@ -33,18 +35,24 @@ plot_owner_perc <- function(race) {
       values = c("#ffe6b3", "#cc9966"),
       labels = NULL
     ) +
-    theme(legend.position = "none") + 
-    geom_curve(
-      aes(x = 8, xend = 5, y = 0.5, yend = -2),
-      colour = "#cc9966",
-      curvature = -0.4
-    ) +
-    geom_curve(
-      aes(x = 20.5, xend = 20.5, y = 2, yend = -1),
-      colour = "#ffe6b3",
-      curvature = -0.4
-    )
+    theme(legend.position = "none")
 
 }
 
-plot_owner_perc("White")
+plot_white <- plot_owner_perc("White") + 
+  geom_curve(
+    aes(x = 8, xend = 5, y = 0.5, yend = -2),
+    colour = "#cc9966",
+    curvature = -0.4
+  ) +
+  geom_curve(
+    aes(x = 20.5, xend = 20.5, y = 2, yend = -1),
+    colour = "#ffe6b3",
+    curvature = -0.4
+  )
+
+plot_hispanics <- plot_owner_perc("Hispanic")
+plot_black <- plot_owner_perc("Black")
+
+
+plot_hispanics + plot_white + plot_black
