2ef9037030f8b394571b1e321a6f8e065165e544
etiennebacher <you@example.com>
2021-02-16 09:51:30 +0100

    add bracket below plot

--- R/2021-W08-Dubois-challenge/2021-W08-Dubois-challenge.R
+++ R/2021-W08-Dubois-challenge/2021-W08-Dubois-challenge.R
@@ -6,6 +6,7 @@ library(glue)
 library(ggtext)
 library(pdftools)
 library(extrafont)
+library(pBrackets)
 
 ### Try to reproduce https://github.com/ajstarks/dubois-data-portraits/blob/master/challenge/challenge01/original-plate-07.jpg
 
@@ -13,7 +14,7 @@ tuesdata <- tidytuesdayR::tt_load(2021, week = 8)
 
 # extrafont::font_import("/home/etienne/Téléchargements/Orbitron/")
 
-tuesdata$georgia_pop %>%
+p <- tuesdata$georgia_pop %>%
   pivot_longer(
     cols = !Year,
     names_to = "Color",
@@ -24,9 +25,9 @@ tuesdata$georgia_pop %>%
   scale_linetype_manual(values = c("solid", "longdash"), labels = c("COLORED", "WHITE")) +
   scale_x_continuous(breaks = seq(1790, 1890, 10), expand = c(0, 0)) +
   scale_y_continuous(trans = "reverse", breaks = seq(0, 100, 5), expand = c(0, 0)) +
-  coord_flip() +
+  coord_flip(clip = "off") +
   theme(
-    # text = element_text(family = "Orbitron-ExtraBold"),
+    text = element_text(family = "B52-ULCW00-ULC"),
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank(),
     panel.grid.major.x = element_line(color = "#ffc7b3"),
@@ -37,6 +38,7 @@ tuesdata$georgia_pop %>%
     axis.ticks.x = element_blank(),
     axis.ticks.y = element_blank(),
     axis.text = element_text(colour = "#808080"),
+    axis.title.x = element_blank(),
     axis.title.y = element_blank(),
     legend.position = "bottom",
     legend.title = element_blank(),
@@ -45,6 +47,25 @@ tuesdata$georgia_pop %>%
     legend.text = element_text(margin = margin(r = 17, unit = "cm")),
     legend.key.width = unit(2.3, "cm"),
     legend.box.margin = margin(l = 17, unit = "cm"),
-    plot.margin = margin(l = 5, r = 5, unit = "cm")
+    plot.margin = margin(l = 5, r = 5, b = 1, unit = "cm")
   ) +
   labs(title = toupper("\ncomparative increase of white and colored\npopulation in georgia.\n\n")) 
+
+
+# https://stackoverflow.com/a/35662327
+
+bracketsGrob <- function(...){
+  l <- list(...)
+  e <- new.env()
+  e$l <- l
+  grid:::recordGrob(  {
+    do.call(grid.brackets, l)
+  }, e)
+}
+b1 <- bracketsGrob(1.02, -0.16,-0.02, -0.16, curvature = 0.5, lwd = 1, col = "#808080")
+
+p +
+  annotation_custom(b1)
+
+
+
