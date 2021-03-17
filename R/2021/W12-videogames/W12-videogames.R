882e513a6a00a2629fb5df344d85844f1725fcbe
etiennebacher <you@example.com>
2021-03-17 20:08:29 +0100

    add area and change limits

--- R/2021/W12-videogames/W12-videogames.R
+++ R/2021/W12-videogames/W12-videogames.R
@@ -19,31 +19,36 @@ data_clean <- tuesdata$games %>%
   select(x_axis, year, tot_players) %>% 
   distinct()
 
-options(scipen = 999)
-
 data_clean %>% 
   ggplot(aes(x = x_axis, y = tot_players)) +
-  geom_line(color = "#e69900") +
-  scale_x_continuous(breaks = seq(2012, 2021, 2)) +
-  scale_y_continuous(labels = paste0(seq(0, 5, 1), "M"), 
-                     limits = c(0, 5000000)) +
+  geom_line(color = "#b37700") +
+  geom_area(fill = "#333333") +
+  scale_x_continuous(
+    breaks = seq(2013, 2021, 1),
+    limits = c(2012.56, 2021.16)
+  ) +
+  scale_y_continuous(
+    labels = paste0(seq(0, 5, 1), "M"), 
+    limits = c(0, 5000000)
+  ) +
   labs(
     y = "Average number of players\n",
     title = "Average number of players simultaneously playing on Steam"
   ) +
   theme(
     axis.title.x = element_blank(),
-    axis.title.y = element_text(color = "#e69900"),
-    axis.text.x = element_text(color = "#e69900"),
-    axis.text.y = element_text(color = "#e69900"),
+    axis.title.y = element_text(color = "#b37700"),
+    axis.text.x = element_text(color = "#333333"),
+    axis.text.y = element_text(color = "#333333"),
     axis.ticks = element_blank(),
     panel.background = element_rect(fill = "#ffeecc"),
     plot.background = element_rect(fill = "#ffeecc"),
     panel.grid.major = element_line(color = "#ffe6b3"),
     panel.grid.minor = element_blank(),
     axis.line = element_line(color = "#ffe6b3"),
-    plot.title = element_text(hjust = 0.5, color = "#e69900",
-                              size = 15)
+    plot.title = element_text(hjust = 0.5, color = "#b37700",
+                              size = 15),
+    text = element_text(family = "Roboto Mono")
   ) +
   geom_curve(
     aes(x = 2016.3, xend = 2017.2, y = 3500000, yend = 2400000),
@@ -62,28 +67,28 @@ data_clean %>%
     x = 2015,
     y = 3500000,
     label = paste0('Release of "Player', "'s \nUnknown Battlegrounds", '"'),
-    color = "#e69900"
+    color = "#b37700"
   ) +
   annotate(
     "text",
     x = 2019.8,
     y = 1100000,
     label = "Start of anti-Covid \nmeasures",
-    color = "#e69900"
+    color = "#b37700"
   ) +
   annotate(
     "text",
     x = 2020.32,
     y = 4680000,
     label = "4.527M",
-    color = "#e69900"
+    color = "#b37700"
   ) +
   annotate(
     "text",
     x = 2018.08,
     y = 4250000,
     label = "4.102M",
-    color = "#e69900"
+    color = "#b37700"
   )
   
   
