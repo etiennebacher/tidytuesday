56fb648508831dc36b47eb12561bbf52e463dffa
etiennebacher <you@example.com>
2021-04-07 19:58:29 +0200

    init week 15

---
+++ R/2021/W15-deforestation/W15-deforestation.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(ggtext)
+library(tidytuesdayR)
+library(pdftools)
+
+
+
+##########
+## Treat data ##
+##########
+
+tuesdata <- tidytuesdayR::tt_load(2021, week = 15)
