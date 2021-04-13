f506d4c114a6b9e59b24d9c4d834e316b9db110b
etiennebacher <you@example.com>
2021-04-13 17:38:13 +0200

    make cumsum for postal office per state-year

--- R/2021/W16-us-post-offices/W16-us-post-offices.R
+++ R/2021/W16-us-post-offices/W16-us-post-offices.R
@@ -16,6 +16,7 @@ library(packcircles)
 
 tuesdata <- tidytuesdayR::tt_load(2021, week = 16)
 
+
 clean_data <- tuesdata$post_offices %>%
   select(state, established) %>% 
   rename(year = established) %>% 
@@ -23,26 +24,55 @@ clean_data <- tuesdata$post_offices %>%
   group_by(state, year) %>% 
   mutate(n = n()) %>% 
   ungroup() %>% 
-  distinct()
+  distinct() %>% 
+  arrange(state, year) %>% 
+  group_by(state) %>% 
+  mutate(
+    cumsum = cumsum(n)
+  ) 
+
+# Create all combinations state-year possible in a separate df and 
+# merge it with the data
+complete_data <- data.frame(
+  state = rep(unique(clean_data$state), each = 362),
+  year = rep(seq(min(clean_data$year), 
+                 max(clean_data$year), 
+                 1), 
+             times = length(unique(clean_data$state)))
+)
+
+# Same value for cumsum if NA and if there was a value earlier
+# e.g if NA in 1883 and value in 1882, then value in 1883 = value in 1883
+# If only NA before, set to 0
+clean_data <- left_join(complete_data, clean_data, 
+                        by = c("year", "state")) %>% 
+  group_by(state) %>% 
+  fill(n, cumsum, .direction = "down") %>% 
+  mutate(
+    n = replace_na(n, 0),
+    cumsum = replace_na(cumsum, 0)
+  )
 
 
-packing <- circleProgressiveLayout(clean_data$n, sizetype = 'area')
-
-clean_data <- cbind(clean_data, packing)
-
+packing <- circleProgressiveLayout(clean_data %>% 
+                                     filter(year == 1900) %>% 
+                                     pull(cumsum), 
+                                   sizetype = 'area')
+foo <- cbind(clean_data %>% 
+                      filter(year == 1900), packing)
 
 clean_data_mod <- circleLayoutVertices(packing, npoints = 50)
 
 ggplot() + 
   geom_polygon(
     data = clean_data_mod, 
-    aes(x, y, group = id, fill = as.factor(id)),
+    aes(x, y, group = state, fill = as.factor(state)),
     colour = "black", 
     alpha = 0.6
   ) +
   
   # Add text in the center of each bubble + control its size
-  geom_text(data = clean_data, aes(x, y, size = n, label = state)) +
+  geom_text(data = foo, aes(x, y, size = cumsum, label = state)) +
   scale_size_continuous(range = c(1,4)) +
   
   # General theme:
