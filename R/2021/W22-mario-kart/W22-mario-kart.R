900d861b9525c9d67a9f409dd92ee4fd32c17f5d
etiennebacher <etienne.bacher@protonmail.com>
2021-05-25 20:21:46 +0200

    theme week 22

--- R/2021/W22-mario-kart/W22-mario-kart.R
+++ R/2021/W22-mario-kart/W22-mario-kart.R
@@ -8,6 +8,11 @@ library(pdftools)
 # Takes a few minutes
 # extrafont::font_import()
 
+
+##########
+## Treat the data ##
+##########
+
 tuesdata <- tidytuesdayR::tt_load(2021, week = 22)
 
 data_used <- tuesdata$records %>% 
@@ -19,12 +24,35 @@ most_freq_tracks <- data_used %>%
   head(5) %>% 
   .[, track] 
 
-data_used[track %in% most_freq_tracks] %>% 
+data_cleaned <- data_used[track %in% most_freq_tracks] %>% 
   .[, .SD[which.min(time)], by = .(track, shortcut)] %>% 
   .[, .(track, shortcut, time)] %>% 
   dcast(., track ~ shortcut, value.var = "time") %>%
-  .[, .(diff_time = No-Yes), by = .(track, Yes, No)] %>% 
+  .[, .(diff_time = No-Yes), by = .(track, Yes, No)] 
+
+
+##########
+## Make the plot ##
+##########
+
+data_cleaned %>% 
   ggplot(aes(fct_reorder(track, diff_time), group = 1)) +
-  geom_line(aes(y = No)) +
-  geom_line(aes(y = Yes)) +
-  geom_ribbon(aes(ymin = No, ymax = Yes))
+  geom_line(aes(y = No), color = "#ff0000") +
+  geom_line(aes(y = Yes), color = "#00b33c") +
+  geom_ribbon(aes(ymin = No, ymax = Yes), fill = "#d9e6f2") +
+  ylab("Number of seconds") +
+  scale_color_manual(
+    values = c("Without shortcuts" = "#ff0000", "With shortcuts" = "#00b33c")
+  ) +
+  theme(
+    plot.background = element_rect(fill = "#e6f7ff"),
+    panel.background = element_rect(fill = "#e6f7ff"),
+    axis.title.x = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = "bottom"
+  ) +
+  labs(
+    title = "How much time do shortcuts save?",
+    subtitle = paste0("On the five most played tracks, shortcuts have saved in average ", mean(data_cleaned$diff_time), " seconds."),
+    caption = "\nMade by Etienne Bacher, data from Benedikt Claus"
+  )
