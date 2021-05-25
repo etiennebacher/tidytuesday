85d40e5793a00527f24d1de849c802b8f33f4aae
etiennebacher <you@example.com>
2021-05-25 19:06:41 +0200

    start week 22

--- R/2021/W22-mario-kart/W22-mario-kart.R
+++ R/2021/W22-mario-kart/W22-mario-kart.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(ggplot2)
+library(forcats)
 library(tidytuesdayR)
 library(extrafont)
 library(pdftools)
@@ -8,3 +9,22 @@ library(pdftools)
 # extrafont::font_import()
 
 tuesdata <- tidytuesdayR::tt_load(2021, week = 22)
+
+data_used <- tuesdata$records %>% 
+  as.data.table()
+
+most_freq_tracks <- data_used %>% 
+  .[, .(.N), by = .(track)] %>% 
+  .[order(-N)] %>% 
+  head(5) %>% 
+  .[, track] 
+
+data_used[track %in% most_freq_tracks] %>% 
+  .[, .SD[which.min(time)], by = .(track, shortcut)] %>% 
+  .[, .(track, shortcut, time)] %>% 
+  dcast(., track ~ shortcut, value.var = "time") %>%
+  .[, .(diff_time = No-Yes), by = .(track, Yes, No)] %>% 
+  ggplot(aes(fct_reorder(track, diff_time), group = 1)) +
+  geom_line(aes(y = No)) +
+  geom_line(aes(y = Yes)) +
+  geom_ribbon(aes(ymin = No, ymax = Yes))
