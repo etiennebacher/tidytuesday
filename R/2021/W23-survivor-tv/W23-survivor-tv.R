222b4ba698a785e06d1fa325e54161c9999efaf4
etiennebacher <you@example.com>
2021-06-03 14:55:41 +0200

    theming week 23

--- R/2021/W23-survivor-tv/W23-survivor-tv.R
+++ R/2021/W23-survivor-tv/W23-survivor-tv.R
@@ -12,7 +12,7 @@ library(pdftools)
 
 
 ##########
-## Treat the data ##
+## Treat the data and make the plot ##
 ##########
 
 clean_data <- as.data.table(viewers) %>% 
@@ -21,7 +21,12 @@ clean_data <- as.data.table(viewers) %>%
   .[, mean_viewers := mean(viewers, na.rm = T), by = season]
 
 ggplot(clean_data, aes(x = viewers, y = season)) +
-  geom_density_ridges() +
+  geom_density_ridges(
+    quantile_lines = TRUE,
+    quantiles = 2,
+    vline_color = "white",
+    fill = "#804000"
+  ) +
   # stat_density_ridges(quantile_lines = TRUE, quantiles = 2)
   scale_x_continuous(
     breaks = seq(5, 30, 5), 
@@ -31,17 +36,36 @@ ggplot(clean_data, aes(x = viewers, y = season)) +
   labs(
     x = "Number of viewers",
     y = "Season",
-    title = '"The Survivor": a less and less popular show',
+    title = '"The Survivor": a less and less popular TV show',
     caption = "Made by Etienne Bacher &middot; Data from the survivoR package"
+  ) +
+  geom_curve(
+    aes(x = 21.5, xend = 20.44, y = 5.9, yend = 6.9),
+    arrow = arrow(length = unit(2, "mm")),
+    colour = "white",
+    size = 0.5,
+    curvature = -0.4
+  ) +
+  geom_textbox(
+    aes(
+      x = 23.5,
+      y = 5.5
+    ),
+    label = "Average number of \nviewers during the season",
+    color = "white",
+    fill = "#804000"
   ) +
   theme(
     plot.background = element_rect(fill = "#cc6600"),
     panel.background = element_rect(fill = "#cc6600"),
     plot.caption = element_markdown(),
     text = element_text(color = "white"),
-    axis.title = element_text(color = "white"),
+    axis.title = element_text(color = "white", size = 13),
     axis.text = element_text(color = "white"),
     panel.grid.major.y = element_blank(),
     panel.grid.minor = element_blank(),
-    axis.ticks = element_blank()
+    panel.grid.major.x = element_line(linetype = "dotted"),
+    axis.ticks = element_blank(),
+    plot.title = element_text(size = 25, hjust = 0.5)
   )
+ 
