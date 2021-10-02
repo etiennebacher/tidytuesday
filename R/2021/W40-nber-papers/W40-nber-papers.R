fc77e2cdb8253b9df2569eb56e1d0b5560e464d5
etiennebacher <etienne.bacher@protonmail.com>
2021-10-02 21:42:43 +0200

    fix week 40

--- R/2021/W40-nber-papers/W40-nber-papers.R
+++ R/2021/W40-nber-papers/W40-nber-papers.R
@@ -40,6 +40,44 @@ joined_df <- left_join(papers, paper_authors) %>%
   ) 
 
 
+# Outputs a dataset with the number of papers that majoritarily concerns
+# one of the three main topics (micro, macro or finance) per year
+
+cleaned_data <- 
+  # Compute the importance of topic per paper
+  joined_df |> 
+  filter(year >= 1980, year != 2021) |> 
+  select(year, paper, program, program_desc, program_category) |> 
+  distinct() |> # Eliminate duplicates due to several authors
+  group_by(year, paper, program_category) |> 
+  count() |> 
+  ungroup() |> 
+  group_by(paper) |> 
+  mutate(share_cat = n / sum(n) * 100) |> 
+  ungroup() |> 
+  
+  # Create the majority topic
+  mutate(majority_cat = case_when(
+    program_category == "Micro" & share_cat > 50 ~ "Micro",
+    program_category == "Macro/International" & 
+      share_cat > 50 ~ "Macro/International",
+    program_category == "Finance" & share_cat > 50 ~ "Finance",
+    TRUE ~ NA_character_
+  )) |> 
+  group_by(paper) |> 
+  fill(majority_cat, .direction = "updown") |> 
+  ungroup() |> 
+  drop_na() |> # Eliminate papers without a majority
+  select(year, paper, majority_cat) |> 
+  distinct() |> 
+  
+  # Compute the number of papers with each majority per year
+  group_by(year, majority_cat) |> 
+  count() |> 
+  ungroup()
+
+
+
 #######################################
 ##### Make plots #####
 #######################################
@@ -61,17 +99,11 @@ custom_theme <- theme(
 )
 
 share_plot <-
-  joined_df |> 
-  filter(year >= 1980, year != 2021) |> 
-  select(year, paper, program_category) |> 
-  distinct() |> 
-  group_by(year, program_category) |> 
-  count() |> 
-  ungroup() |> 
+  cleaned_data |> 
   group_by(year) |> 
-  mutate(share = n / sum(n) * 100) |> 
+  mutate(share = n/sum(n)*100) |> 
   ungroup() |> 
-  ggplot(aes(year, share, fill = program_category)) +
+  ggplot(aes(year, share, fill = majority_cat)) +
   geom_bar(stat = "identity", width = 1) +
   geom_hline(yintercept = 50, linetype = "dashed",
              color = got_palettes$daenerys[2]) +
@@ -94,22 +126,17 @@ share_plot <-
 
 
 evol_plot <-
-  joined_df |> 
-  filter(year >= 1980, year != 2021) |> 
-  select(year, paper, program_category) |> 
-  distinct() |> 
-  group_by(year, program_category) |> 
-  count() |> 
-  ungroup() |> 
-  drop_na() |> 
-  ggplot(aes(year, n, color = program_category)) +
+  cleaned_data |> 
+  ggplot(aes(year, n, color = majority_cat)) +
   geom_line(size = 1.5) +
-  geom_hline(yintercept = 400, alpha = 0.3, linetype = "dashed") +
-  geom_hline(yintercept = 800, alpha = 0.3, linetype = "dashed") +
-  geom_hline(yintercept = 1200, alpha = 0.3, linetype = "dashed") +
+  geom_hline(yintercept = 250, alpha = 0.3, linetype = "dashed") +
+  geom_hline(yintercept = 500, alpha = 0.3, linetype = "dashed") +
+  geom_hline(yintercept = 750, alpha = 0.3, linetype = "dashed") +
+  geom_hline(yintercept = 1000, alpha = 0.3, linetype = "dashed") +
   labs(
     title = "Number of new papers by category",
     color = "Category",
+    x = "",
     y = ""
   ) +
     scale_color_manual(values = got_palettes$daenerys[c(1, 4, 3)]) +
@@ -125,10 +152,12 @@ evol_plot <-
 
 share_plot + evol_plot +
   plot_annotation(
-    title = 'Macro or Micro: which one is the NBER favorite?<br>',
+    title = 'Macro or Micro: which one is the NBER favorite?',
+    subtitle = "Each NBER paper covers at least one topic in micro, macro, or finance. The papers <br> included in the plots below are only those that have a majority category. For instance, <br> a paper whose topics are 50% micro and 50% macro is not included in the plotted data.<br>",
     caption = '<br>Made by Etienne Bacher | Data from National Bureau of Economic Research (NBER)',
     theme = theme(
       plot.title = element_markdown(hjust = 0.5, size = 22),
+      plot.subtitle = element_markdown(hjust = 0.5, size = 12),
       plot.caption = element_markdown(hjust = 0.5),
       plot.background = element_rect(fill = got_palettes$daenerys[5]),
       panel.background = element_rect(fill = got_palettes$daenerys[5]),
